4d1268af40e163205a34a26062db1269a09d86a6
Ado555 <116297586+Ado555@users.noreply.github.com>
2023-02-16 13:33:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+//oppgave 1
+
+// var tall = Math.round(Math.random()*100);
+// var gjett = prompt("Gjett et tall mellom 0 og 100!");
+//
+// while (gjett != tall) {
+// if (gjett < tall) {
+// gjett = prompt("Skriv et høyere tall!");
+// }
+// else {
+// gjett = prompt("Skriv et mindre tall som!");
+// }
+// }
+//
+// alert("Gratulerer, tallet er " + tall + "!");
+
+// oppgave 2
+
+//for (let i = 0; i < 10; i++) {
+//  for (let s = 0; s < 10; s++) {
+//    document.write(s + " ");
+//  }
+//  document.write("<br>");
+//}
+//for (let i = 0; i < 10; i++) {
+//  console.log(" 0 1 2 3 4 5 6 7 8 9");
+//}
